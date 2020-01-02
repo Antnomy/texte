20ee91a46863c18f51025031cdd8ac986eba4d0d
Antônio José <anj_exp@msn.com>
2020-01-02 11:01:29 -0300

    Progeto iniciado mostrando a mensagem ola mundo

---
+++ texta_Git/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace texta_Git
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
